6b1d750d834b0d8e8950150ef0a7398581fe0fc5
Shanay Murdock <murdock.shanay@gmail.com>
2020-03-21 16:26:36 -0400

    Updates

---
+++ precache-manifest.769feac4e2ac767ea9d0b94ed112127e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6587d519efe25b86d8709ce45243ee1b",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "8f9d1b9d0ea8f31bad2f",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "96a27bedcc0f5c9d53ad",
+    "url": "/robofriends/static/css/main.8610cfbc.chunk.css"
+  },
+  {
+    "revision": "8f9d1b9d0ea8f31bad2f",
+    "url": "/robofriends/static/js/2.5b396b40.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/robofriends/static/js/2.5b396b40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96a27bedcc0f5c9d53ad",
+    "url": "/robofriends/static/js/main.7bc87c15.chunk.js"
+  },
+  {
+    "revision": "5677d8ff5cb4c373cfaa",
+    "url": "/robofriends/static/js/runtime-main.c712519d.js"
+  }
+]);
